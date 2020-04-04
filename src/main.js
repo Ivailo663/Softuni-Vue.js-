58de4e7b01a199e76319fb4229657ff0ce5f1742
Georgiev <igeorgiev21@dxc.com>
2020-04-05 01:55:57 +0300

    minor changes

--- src/main.js
+++ src/main.js
@@ -7,9 +7,14 @@ import "../public/styles/style.less";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
 
 new Vue({
   router,
   store,
+  Vuetify,
   render: (h) => h(App),
 }).$mount("#app");
